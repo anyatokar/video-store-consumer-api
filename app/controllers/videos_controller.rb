f3d3261fceee3bdc671e914a31f517ef88a0029b
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-01-22 10:07:02 -0800

    added image_url to show of Video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -15,7 +15,7 @@ class VideosController < ApplicationController
     render(
       status: :ok,
       json: @video.as_json(
-        only: [:title, :overview, :release_date, :inventory],
+        only: [:title, :overview, :release_date, :inventory, :image_url],
         methods: [:available_inventory]
         )
       )
